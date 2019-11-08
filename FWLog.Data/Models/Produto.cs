021f9cac0ba2811a1bfd70ce86c5d85d999ee75f
Victor Ozório <victor.ozorio@dartdigital.com.br>
2019-11-08 15:27:07 -0300

    Criação dos scripts e implementação da tela, criação do filtro e classes do banco de dados.

---
+++ FWLog.Data/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace FWLog.Data.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Descricao { get; set; }
+        public string Referencia { get; set; }
+        public int IdUnidadeMedida { get; set; }
+        public decimal Peso { get; set; }
+
+        public virtual UnidadeMedida UnidadeMedida { get; set; }
+    }
+}
