bf9b4da4c1c30589206230cc0432238892d5354a
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2019-11-12 14:10:47 -0300

    Inicio entidade de conferência

--- FWLog.Web.Backoffice/Models/BORecebimentoNotaCtx/BODetalhesEtiquetaConferenciaViewModel.cs
+++ FWLog.Web.Backoffice/Models/BORecebimentoNotaCtx/BODetalhesEtiquetaConferenciaViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace FWLog.Web.Backoffice.Models.BORecebimentoNotaCtx
+﻿namespace FWLog.Web.Backoffice.Models.BORecebimentoNotaCtx
 {
     public class BODetalhesEtiquetaConferenciaViewModel
     {
@@ -11,5 +6,20 @@ namespace FWLog.Web.Backoffice.Models.BORecebimentoNotaCtx
         public string NumeroNotaFiscal { get; set; }
         public string DataHoraRecebimento { get; set; }
         public string QuantidadeVolumes { get; set; }
+        public string Referencia { get; set; }
+        public string Embalagem { get; set; }
+        public string Unidade { get; set; }
+        public string Multiplo { get; set; }
+        public int QuantidadePorVolumes { get; set; }
+        public int NumerosDeCaixas { get; set; }
+        public int TotalItens { get; set; }
+        public decimal MediaVendaMes { get; set; }
+        public int EStoque { get; set; }
+        public string Localizacao { get; set; }
+        public int QuantidadeNaoConferida { get; set; }
+        public int QuantidadeConferida { get; set; }
+        public int ReservaVendas { get; set; }
+        public int Apoio { get; set; }
+
     }
 }
