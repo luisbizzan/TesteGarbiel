34ecaeb2125f0e02c1fb4db91c9b1d0ab0a83010
Victor Ozório <victor.ozorio@dartdigital.com.br>
2019-11-12 13:34:16 -0300

    Alterações da tarefa.

--- FWLog.Data/Models/Lote.cs
+++ FWLog.Data/Models/Lote.cs
@@ -7,20 +7,21 @@ namespace FWLog.Data.Models
     public class Lote
     {
         [Key]
-        public int IdLote { get; set; }
-        public int IdLoteStatus { get; set; }
-        public int IdNotaFiscal { get; set; }
-        public DateTime DataCompra { get; set; }
-        public DateTime DataRecebimento { get; set; }
-        public int QuantidadePeca { get; set; }
-        public int QuantidadeVolume { get; set; }
-        public string RecebidoPor { get; set; }
-        public string ConferidoPor { get; set; }
+        public int? IdLote { get; set; }
+        public int? IdLoteStatus { get; set; }
+        public int? IdNotaFiscal { get; set; }
+        public DateTime? DataCompra { get; set; }
+        public DateTime? DataRecebimento { get; set; }
+        public int? QuantidadePeca { get; set; }
+        public int? QuantidadeVolume { get; set; }
+        public string IdUsuarioRecebimento { get; set; }
 
         [ForeignKey(nameof(IdLoteStatus))]
         public LoteStatus LoteStatus { get; set; }
 
         [ForeignKey(nameof(IdNotaFiscal))]
         public NotaFiscal NotaFiscal { get; set; }
+
+        public AspNetUsers UsuarioRecebimento { get; set; }
     }
 }
