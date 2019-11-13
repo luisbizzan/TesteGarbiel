2b6f9525eb1f2882057f46783f20c27cb1318e7f
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2019-11-13 14:46:59 -0300

    Alterações na exibição do recebimento de nota fiscal e gravação do lote.

---
+++ FWLog.Services/Services/ApplicationLogService.cs
@@ -0,0 +1,27 @@
+﻿using FWLog.Data;
+using System.Transactions;
+
+namespace FWLog.Services.Services
+{
+    public class ApplicationLogService
+    {
+        private UnitOfWork _unitOfWork;
+
+        public ApplicationLogService(UnitOfWork uow)
+        {
+            _unitOfWork = uow;
+        }
+
+        public void Add(ApplicationLog applicationLog)
+        {
+            using (TransactionScope transactionScope = _unitOfWork.CreateTransactionScope())
+            {
+                _unitOfWork.ApplicationLogRepository.Add(applicationLog);
+
+                _unitOfWork.SaveChanges();
+                transactionScope.Complete();
+            }
+        }
+
+    }
+}
