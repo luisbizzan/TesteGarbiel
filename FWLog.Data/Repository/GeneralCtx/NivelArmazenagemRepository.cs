224dfff6114ac51508786aec77bb9dd98244b7a7
Leandro Gularte <leandro.gularte@dartdigital.com.br>
2019-11-27 16:59:06 -0300

    123

--- FWLog.Data/Repository/GeneralCtx/NivelArmazenagemRepository.cs
+++ FWLog.Data/Repository/GeneralCtx/NivelArmazenagemRepository.cs
@@ -51,10 +51,9 @@ namespace FWLog.Data.Repository.GeneralCtx
             totalRegistros = Entities.NivelArmazenagem.Where(w => w.IdEmpresa == filtros.CustomFilter.IdEmpresa).Count();
 
             var query = Entities.NivelArmazenagem
-                .Where(w => 
-                w.IdEmpresa == filtros.CustomFilter.IdEmpresa && 
-                //(filtros.CustomFilter.Descricao.Equals(string.Empty) || w.Descricao.Contains(filtros.CustomFilter.Descricao)) &&
-                (!filtros.CustomFilter.Ativo.HasValue || w.Ativo == filtros.CustomFilter.Ativo.Value))
+                .Where(w => w.IdEmpresa == filtros.CustomFilter.IdEmpresa && 
+                (filtros.CustomFilter.Descricao.Equals(string.Empty) || w.Descricao.Contains(filtros.CustomFilter.Descricao)) &&
+                (filtros.CustomFilter.Ativo.HasValue == false || w.Ativo == filtros.CustomFilter.Ativo.Value))
                 .Select(s => new NivelArmazenagemPesquisaModalListaLinhaTabela
                 {
                     IdNivelArmazenagem = s.IdNivelArmazenagem,
