c20ced13a1489fe20c8aa2b0a86c2faf0093c336
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2019-12-26 17:41:01 -0300

    Tela Resumo de Produção

---
+++ FWLog.Web.Backoffice/Models/BORecebimentoNotaCtx/RelatorioResumoProducaoViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FWLog.Web.Backoffice.Models.BORecebimentoNotaCtx
+{
+    public class RelatorioResumoProducaoViewModel
+    {
+        public RelatorioResumoProducaoRecebimentoListItemViewModel ListaRecebimento { get; set; }
+
+        public RelatorioResumoProducaoConferenciaListItemViewModel ListaConferencia { get; set; }
+
+        public RelatorioResumoProducaoFilterViewModel Filter { get; set; }
+
+        public RelatorioResumoProducaoViewModel()
+        {
+            ListaRecebimento = new RelatorioResumoProducaoRecebimentoListItemViewModel();
+            ListaConferencia = new RelatorioResumoProducaoConferenciaListItemViewModel();
+            Filter = new RelatorioResumoProducaoFilterViewModel();
+        }
+    }
+
+    public class RelatorioResumoProducaoRecebimentoListItemViewModel
+    {
+        [Display(Name = "Notas Recebidas")]
+        public long NotasRecebidas { get; set; }
+
+        [Display(Name = "Peças Recebidas")]
+        public long PecasRecebidas { get; set; }
+
+        [Display(Name = "Ranking ")]
+        public long Ranking { get; set; }
+
+        [Display(Name = "Usuário")]
+        public string NomeUsuario { get; set; }
+
+        [Display(Name = "Notas Recebidas (Usuario)")]
+        public long NotasRecebidasUsuario { get; set; }
+
+        [Display(Name = "Peças Recebidas (Usuario)")]
+        public long PecasRecebidasUsuario { get; set; }
+
+        [Display(Name = "Percentual")]
+        public decimal Percentual { get; set; }
+    }
+
+    public class RelatorioResumoProducaoConferenciaListItemViewModel
+    {
+        [Display(Name = "Lotes Recebidas")]
+        public long LotesRecebidas { get; set; }
+
+        [Display(Name = "Peças Recebidas")]
+        public long PecasRecebidas { get; set; }
+
+        [Display(Name = "Ranking ")]
+        public long Ranking { get; set; }
+
+        [Display(Name = "Usuário")]
+        public string NomeUsuario { get; set; }
+
+        [Display(Name = "Lotes Recebidas (Usuario)")]
+        public long LotesRecebidasUsuario { get; set; }
+
+        [Display(Name = "Peças Recebidas (Usuario)")]
+        public long PecasRecebidasUsuario { get; set; }
+
+        [Display(Name = "Percentual")]
+        public decimal Percentual { get; set; }
+    }
+
+    public class RelatorioResumoProducaoFilterViewModel
+    {
+        public string IdUsuario { get; set; }
+
+        [Display(Name = "Data")]
+        public DateTime DataRecebimentoMinima { get; set; } = DateTime.Today;
+
+        [Display(Name = "Data")]
+        public DateTime? DataRecebimentoMaxima { get; set; }
+
+
+    }
+}
---
+++ FWLog.Web.Backoffice/Scripts/Views/BORecebimentoNota/relatorioResumoProducao.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    $('#dataTable').DataTable({
+        ajax: {
+            "url": view.RecebimentopageDataUrl,
+            "type": "POST",
+            "data": function (data) {
+                dart.dataTables.saveFilterToData(data);
+            },
+            "error": function (data) {
+                if (!!(data.statusText)) {
+                    PNotify.error({ text: data.statusText });
+                    NProgress.done();
+                }
+            }
+        },
+        //deferLoading: 0,
+        initComplete: function (settings, json) {
+            dart.dataTables.addEventsForDropdownAutoposition($('#dataTable'));
+        },
+        stateSaveParams: function (settings, data) {
+            dart.dataTables.saveFilterToData(data);
+        },
+        stateLoadParams: function (settings, data) {
+            dart.dataTables.loadFilterFromData(data);
+        },
+        columns: [
+            { data: 'NomeUsuario' },
+            { data: 'NotasRecebidasUsuario' },
+            { data: 'PecasRecebidasUsuario' },
+            { data: 'NotasRecebidas', width: 100 },
+            { data: 'PecasRecebidas', width: 100 },
+            { data: 'Percentual', width: 100 },
+            { data: 'Ranking', width: 100 }
+        ]
+    });
+
+    $('#dataTable').dataTable.error = function (settings, helpPage, message) {
+        console.log(message)
+    };
+})();
