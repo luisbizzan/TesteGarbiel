2521d48c9e628194e99785fd167a155f7e9de765
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2020-01-03 09:48:27 -0300

    Histórico Quarentena - Gravar

--- FWLog.Data/Repository/GeneralCtx/QuarenatenaRepository.cs
+++ FWLog.Data/Repository/GeneralCtx/QuarenatenaRepository.cs
@@ -1,15 +1,58 @@
 ﻿using FWLog.Data.Models;
 using FWLog.Data.Repository.CommonCtx;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace FWLog.Data.Repository.GeneralCtx
 {
-    public class QuarentenaRepository : GenericRepository<Quarentena>
+    public class QuarentenaRepository : BaseRepository
     {
-        public QuarentenaRepository(Entities entities) : base(entities) { }
+        readonly IDbSet<Quarentena> _dbSet;
+
+        public QuarentenaRepository(Entities entities) : base(entities)
+        {
+            _dbSet = Entities.Set<Quarentena>();
+        }
+
+        public Quarentena GetById(long id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(Quarentena entity, string IdUsuario)
+        {
+            var log = new QuarentenaHistorico { Data = DateTime.Now, Descricao = "Criação", IdQuarentena = entity.IdQuarentena, IdUsuario = IdUsuario };
+
+            Entities.QuarentenaHistorico.Add(log);
+
+            _dbSet.Add(entity);
+        }
+
+        public void Update(Quarentena entity, string IdUsuario)
+        {
+            var log = new QuarentenaHistorico { Data = DateTime.Now, Descricao = "Atualização", IdQuarentena = entity.IdQuarentena, IdUsuario = IdUsuario };
+
+            Entities.QuarentenaHistorico.Add(log);
+
+            var entry = Entities.Entry(entity);
+            _dbSet.Attach(entity);
+            entry.State = EntityState.Modified;
+        }
+
+        public void AddRange(IEnumerable<Quarentena> entities, string IdUsuario)
+        {
+            foreach (var entity in entities)
+            {
+                var log = new QuarentenaHistorico { Data = DateTime.Now, Descricao = "Criação", IdQuarentena = entity.IdQuarentena, IdUsuario = IdUsuario };
+
+                Entities.QuarentenaHistorico.Add(log);
+
+                _dbSet.Add(entity);
+            }
+        }
 
         public IQueryable<Quarentena> All()
         {
@@ -26,4 +69,5 @@ namespace FWLog.Data.Repository.GeneralCtx
             return _dbSet.Any(predicate);
         }
     }
+
 }
--- FWLog.Web.Backoffice/Controllers/BOQuarentenaController.cs
+++ FWLog.Web.Backoffice/Controllers/BOQuarentenaController.cs
@@ -3,6 +3,7 @@ using FWLog.Data.EnumsAndConsts;
 using FWLog.Data.Models;
 using FWLog.Data.Models.FilterCtx;
 using FWLog.Data.Models.GeneralCtx;
+using FWLog.Services.Model.Relatorios;
 using FWLog.Services.Services;
 using FWLog.Web.Backoffice.Helpers;
 using FWLog.Web.Backoffice.Models.BOQuarentenaCtx;
@@ -271,7 +272,7 @@ namespace FWLog.Web.Backoffice.Controllers
                     entidade.DataEncerramento = DateTime.Now;
                 }
 
-                _uow.QuarentenaRepository.Update(entidade);
+                _uow.QuarentenaRepository.Update(entidade, IdUsuario);
 
                 var userInfo = new BackOfficeUserInfo();
                 _boLogSystemService.Add(new BOLogSystemCreation
@@ -314,7 +315,7 @@ namespace FWLog.Web.Backoffice.Controllers
                 var userLog = new UserLog
                 {
                     IP = userInfo.IP,
-                    UserId = userInfo.UserId
+                    UserId = IdUsuario
                 };
 
                 var request = new TermoResponsabilidadeRequest
@@ -352,7 +353,7 @@ namespace FWLog.Web.Backoffice.Controllers
                 Itens = _uow.QuarentenaHistoricoRepository.QuarentenaHistoricos().Where(x => x.IdQuarentena == id).ToList().Select(x => new HistoricoQuarentenaItemViewModel
                 {
                     Data = x.Data.ToString("dd/MM/yyyy"),
-                    Usuario = x.NomeUsuario, 
+                    Usuario = x.NomeUsuario,
                     Descricao = x.Descricao
                 }).ToList()
             };
