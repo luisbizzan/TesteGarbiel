f7602f9d74f1f093f170552b4a207cd3ee02a6c5
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-01-21 18:06:01 -0300

    Ajuste no cadastro de usuário.

--- FWLog.Web.Backoffice/Scripts/Views/BOAccount/create.js
+++ FWLog.Web.Backoffice/Scripts/Views/BOAccount/create.js
@@ -30,45 +30,46 @@
             return;
         }
 
-        var index = $('.panelEmpresa').length;
-
-        $.post(HOST_URL + CONTROLLER_PATH + "AdicionarEmpresa", { id, index }, function (result) {
+        $.post(HOST_URL + CONTROLLER_PATH + "AdicionarEmpresa", { id }, function (result) {
             $("#EmpresasSelecionadas").append(result);
+            loadButtonsEvents();
         });
     });
 
-    //$('.btnSalvar').on('click', function () {
-    //    $("#EmpresasSelecionadas").find(".panelEmpresa").each(function (i, e) {
-    //        $(this).find(".idEmpresa").attr('id', `EmpresasGrupos_${i}__IdEmpresa`);
-    //        $(this).find(".idEmpresa").attr('name', `EmpresasGrupos[${i}].IdEmpresa`);
+    $('.btnSalvar').on('click', function () {
+        $("#EmpresasSelecionadas").find(".panelEmpresa").each(function (i, e) {
+            $(this).find(".idEmpresa").attr('id', `EmpresasGrupos_${i}__IdEmpresa`);
+            $(this).find(".idEmpresa").attr('name', `EmpresasGrupos[${i}].IdEmpresa`);
 
-    //        $(this).find(".empresaNome").attr('id', `EmpresasGrupos_${i}__Nome`);
-    //        $(this).find(".empresaNome").attr('name', `EmpresasGrupos[${i}].Nome`);
+            $(this).find(".empresaNome").attr('id', `EmpresasGrupos_${i}__Nome`);
+            $(this).find(".empresaNome").attr('name', `EmpresasGrupos[${i}].Nome`);
 
-    //        $(this).find(".isEmpresaPrincial").attr('id', `EmpresasGrupos_${i}__IsEmpresaPrincipal`);
-    //        $(this).find(".isEmpresaPrincial").attr('name', `EmpresasGrupos[${i}].IsEmpresaPrincipal`);
+            $(this).find(".isEmpresaPrincial").attr('id', `EmpresasGrupos_${i}__IsEmpresaPrincipal`);
+            $(this).find(".isEmpresaPrincial").attr('name', `EmpresasGrupos[${i}].IsEmpresaPrincipal`);
 
-    //        $(this).find(".idPerfilImpressoraPadrao").attr('id', `EmpresasGrupos_${i}__IdPerfilImpressoraPadrao`);
-    //        $(this).find(".idPerfilImpressoraPadrao").attr('name', `EmpresasGrupos[${i}].IdPerfilImpressoraPadrao`);
+            $(this).find(".idPerfilImpressoraPadrao").attr('id', `EmpresasGrupos_${i}__IdPerfilImpressoraPadrao`);
+            $(this).find(".idPerfilImpressoraPadrao").attr('name', `EmpresasGrupos[${i}].IdPerfilImpressoraPadrao`);
 
-    //        $(this).find("[data-group]").each(function (ii, ee) {
-    //            $(this).attr('id', `EmpresasGrupos_${i}__Grupos_${ii}__IsSelected`);
-    //            $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].IsSelected`);
-    //        });
+            $(this).find("[data-group]").each(function (ii, ee) {
+                $(this).attr('id', `EmpresasGrupos_${i}__Grupos_${ii}__IsSelected`);
+                $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].IsSelected`);
+            });
 
-    //        $(this).find(".empresaprincipal").find('input[type=hidden]').each(function (ii, ee) {
-    //            $(this).attr('name', `EmpresasGrupos_${i}__IsEmpresaPrincipal`);
-    //        });
+            $(this).find(".empresaprincipal").find('input[type=hidden]').each(function (ii, ee) {
+                $(this).attr('name', `EmpresasGrupos_${i}__IsEmpresaPrincipal`);
+            });
 
-    //        $(this).find(".empresasGrupo").find('input[type=hidden]').not(".NomeGrupo").each(function (ii, ee) {
-    //            $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].IsSelected`);
-    //        });
+            $(this).find(".empresasGrupo").find('input[type=hidden]').not(".NomeGrupo").each(function (ii, ee) {
+                $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].IsSelected`);
+            });
 
-    //        $(this).find(".empresasGrupo").find("input:hidden.NomeGrupo").each(function (ii, ee) {
-    //            $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].Name`);
-    //        });
-    //    });
-    //});
+            $(this).find(".empresasGrupo").find("input:hidden.NomeGrupo").each(function (ii, ee) {
+                $(this).attr('name', `EmpresasGrupos[${i}].Grupos[${ii}].Name`);
+            });
+        });
+
+        loadButtonsEvents();
+    });
 })();
 
 function loadButtonsEvents() {
