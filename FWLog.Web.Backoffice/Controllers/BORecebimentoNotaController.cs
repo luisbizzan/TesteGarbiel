11dc762e4494cd65c9c756a7b155b3a91a4fa860
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2020-01-28 08:49:03 -0300

    BUG - Contagem de peças na Conferência 100%

--- FWLog.Web.Backoffice/Controllers/BORecebimentoNotaController.cs
+++ FWLog.Web.Backoffice/Controllers/BORecebimentoNotaController.cs
@@ -1088,7 +1088,8 @@ namespace FWLog.Web.Backoffice.Controllers
                 QuantidadeNaoConferida = quantidadeNaoConferida,
                 QuantidadeConferida = quantidadeConferida,
                 InicioConferencia = DateTime.Now.ToString(),
-                QuantidadePorCaixa = null
+                QuantidadePorCaixa = null,
+                Multiplo = produto.MultiploVenda
             };
 
             //Se o tipo da conferência for Por Quantidade, atribui 1 para o campo quantidade de caixa.
