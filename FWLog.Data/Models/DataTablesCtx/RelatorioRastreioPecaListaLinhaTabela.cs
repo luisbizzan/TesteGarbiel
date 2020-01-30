03957d77def1bdfb175a15244d9e674480cfbeec
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-01-30 14:27:49 -0300

    Alteração na consulta do relatório de rastreio de peças

--- FWLog.Data/Models/DataTablesCtx/RelatorioRastreioPecaListaLinhaTabela.cs
+++ FWLog.Data/Models/DataTablesCtx/RelatorioRastreioPecaListaLinhaTabela.cs
@@ -6,12 +6,15 @@ namespace FWLog.Data.Models.DataTablesCtx
     {
         public long IdEmpresa { get; set; }
         public string Empresa { get; set; }
+        public LoteStatusEnum IdLoteStatus { get; set; }
+        public long IdNotaFiscal { get; set; }
+        public long IdProduto { get; set; }
         public long IdLote { get; set; }
         public int NroNota { get; set; }
         public string ReferenciaPronduto { get; set; }
         public DateTime DataCompra { get; set; }
         public DateTime DataRecebimento { get; set; }
-        public long? QtdCompra { get; set; }
-        public long? QtdRecebida { get; set; }
+        public int QtdCompra { get; set; }
+        public int QtdRecebida { get; set; }
     }
 }
