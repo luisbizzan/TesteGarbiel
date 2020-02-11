e4550d35b4d9fe1bfcbb3e6e7c268f119fd5fe67
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-02-11 15:44:10 -0300

    Ajuste na chamada do Sankhya

--- FWLog.Services/Services/ProdutoService.cs
+++ FWLog.Services/Services/ProdutoService.cs
@@ -237,6 +237,11 @@ namespace FWLog.Services.Services
 
         public async Task<long> ConsultarQuantidadeReservada(long idProduto, long idEmpresa)
         {
+            if (!Convert.ToBoolean(ConfigurationManager.AppSettings["IntegracaoSankhya_Habilitar"]))
+            {
+                return 0;
+            }
+            
             long quantidadeReservada = 0;
 
             Empresa empresa = _uow.EmpresaRepository.GetById(idEmpresa);
