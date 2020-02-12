b4f08e94c3b9f64fa391c7b18db76024dde9145d
popolinclaudio <claudio.popolin@dartdigital.com.br>
2020-02-12 11:19:39 -0300

    Implementação da tarefa FUR-812

--- FWLog.Web.Backoffice/Scripts/Views/BOAccountBase/logOn.js
+++ FWLog.Web.Backoffice/Scripts/Views/BOAccountBase/logOn.js
@@ -1,12 +1,26 @@
 ﻿(function () {
 
-    $('[data-language]').on('click', function () {        
+    
+
+    $('[data-language]').on('click', function () {
+        debugger
         var selected = $(this).attr('data-language');
         window.location = getLanguageUrl(selected);
     });
 
     var getLanguageUrl = function (culture) {
+        debugger
         return view.logOnUrl + "?l=" + encodeURIComponent(culture);
     };
+
+    $(".form-control").on('keydown', document_Keydown);
+
+    function document_Keydown(e) {
+        
+        if (e.which == 13) {
+            $('#confirm-btn').click();
+            return false;
+        }
+    }
 
 })();
