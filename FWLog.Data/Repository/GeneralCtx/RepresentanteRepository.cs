ccd4a3479a3e431367a262756376cc745ff4ad5a
popolinclaudio <claudio.popolin@dartdigital.com.br>
2020-02-18 15:15:51 -0300

    Criação do Entities, repository e unitofwork

---
+++ FWLog.Data/Repository/GeneralCtx/RepresentanteRepository.cs
@@ -0,0 +1,15 @@
+﻿using FWLog.Data.Models;
+using FWLog.Data.Repository.CommonCtx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWLog.Data.Repository.GeneralCtx
+{
+    public class RepresentanteRepository : GenericRepository<Representante>
+    {
+        public RepresentanteRepository(Entities entities) : base(entities) { }
+    }
+}
