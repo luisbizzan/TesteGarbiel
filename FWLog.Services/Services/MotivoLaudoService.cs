75d5df2a227791299681b7986d7fc3c80b6ed09a
popolinclaudio <claudio.popolin@dartdigital.com.br>
2020-02-21 12:18:10 -0300

    Implementação da tarea FUR-877

---
+++ FWLog.Services/Services/MotivoLaudoService.cs
@@ -0,0 +1,39 @@
+﻿using FWLog.Data;
+using FWLog.Data.Models;
+
+namespace FWLog.Services.Services
+{
+    public class MotivoLaudoService
+    {
+        private readonly UnitOfWork _uow;
+
+        public MotivoLaudoService(UnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public void Add(MotivoLaudo motivoLaudo)
+        {
+            // Adicionar validações se necessário...
+
+            _uow.MotivoLaudoRepository.Add(motivoLaudo);
+            _uow.SaveChanges();
+        }
+
+        public void Edit(MotivoLaudo motivoLaudo)
+        {
+            // Adicionar validações se necessário...
+
+            _uow.MotivoLaudoRepository.Update(motivoLaudo);
+            _uow.SaveChanges();
+        }
+
+        public void Delete(MotivoLaudo motivoLaudo)
+        {
+            // Adicionar validações se necessário...
+
+            _uow.MotivoLaudoRepository.Delete(motivoLaudo);
+            _uow.SaveChanges();
+        }
+    }
+}
