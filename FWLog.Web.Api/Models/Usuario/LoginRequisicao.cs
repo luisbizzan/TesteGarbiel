b24644c69a66a91d70c8bb5a813854bbd303156b
leandrogularte <leandro.gularte@dartdigital.com.br>
2020-02-26 17:32:22 -0300

    permissões

--- FWLog.Web.Api/Models/Usuario/LoginRequisicao.cs
+++ FWLog.Web.Api/Models/Usuario/LoginRequisicao.cs
@@ -1,13 +1,12 @@
-﻿using FWLog.Web.Api.GlobalResources;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FWLog.Web.Api.Models.Usuario
 {
     public class LoginRequisicao
     {
-        [StringLength(maximumLength: 5, MinimumLength = 5, ErrorMessage = "O código do usuario deve ter 5 números.")]
+        [StringLength(maximumLength: 5, MinimumLength = 5, ErrorMessage = "O código deve ter 5 dígitos.")]
         public string Codigo { get; set; }
-        [Required(ErrorMessage = "Informe o código do usuário.")]
+        [Required(ErrorMessage = "Informe a senha.")]
         public string Senha { get; set; }
     }
 }
--- FWLog.Web.Api/Models/Usuario/ValidarUsuarioRequisicao.cs
+++ FWLog.Web.Api/Models/Usuario/ValidarUsuarioRequisicao.cs
@@ -4,7 +4,7 @@ namespace FWLog.Web.Api.Models.Usuario
 {
     public class ValidarUsuarioRequisicao
     {
-        [StringLength(maximumLength: 5, MinimumLength = 5, ErrorMessage = "O código do usuario deve ter 5 números.")]
+        [StringLength(maximumLength: 5, MinimumLength = 5, ErrorMessage = "O código deve ter 5 dígitos.")]
         public string Codigo { get; set; }
     }
 }
