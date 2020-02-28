3a08f8bc68571c36f1317c57010ee33b95db5cf6
Jonatas Dart <jonatas.aguiar@dartdigital.com.br>
2020-02-28 13:43:50 -0300

    Etiqueta Endereço

---
+++ FWLog.Services/Model/Etiquetas/ImprimirEtiquetaEnderecoRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FWLog.Services.Model.Etiquetas
+{
+    public class ImprimirEtiquetaEnderecoRequest
+    {
+        public long IdEnderecoArmazenagem { get; set; }
+        public int QuantidadeEtiquetas { get; set; } = 1;
+        public long IdImpressora { get; set; }
+    }
+}
