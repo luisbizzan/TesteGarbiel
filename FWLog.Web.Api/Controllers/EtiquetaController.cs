d27019357c78f9641172889495833d748c17c02a
leandrogularte <leandro.gularte@dartdigital.com.br>
2020-02-28 10:06:15 -0300

    imprimir endereço armazenagem

---
+++ FWLog.Web.Api/Controllers/EtiquetaController.cs
@@ -0,0 +1,34 @@
+﻿using FWLog.Data;
+using FWLog.Services.Services;
+using FWLog.Web.Api.Models.Etiqueta;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FWLog.Web.Api.Controllers
+{
+    public class EtiquetaController : ApiBaseController
+    {
+        private readonly EtiquetaService _etiquetaService;
+        private readonly UnitOfWork _unitOfWork;
+
+        public EtiquetaController(UnitOfWork unitOfWork, EtiquetaService etiquetaService)
+        {
+            _unitOfWork = unitOfWork;
+            _etiquetaService = etiquetaService;
+        }
+
+        [HttpPost]
+        [Route("api/v1/etiqueta/endereco")]
+        public async Task<IHttpActionResult> ImprimirEtiquetaEndereco(ImprimirEtiquetaEnderecoRequisicao requisicao)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ApiBadRequest(ModelState);
+            }
+
+            //chamar service
+
+            return ApiOk();
+        }
+    }
+}
---
+++ FWLog.Web.Api/Models/Etiqueta/ImprimirEtiquetaEnderecoRequisicao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FWLog.Web.Api.Models.Etiqueta
+{
+    public class ImprimirEtiquetaEnderecoRequisicao
+    {
+        public long IdEnderecoArmazenagem { get; set; }
+        public int IdImpressora { get; set; }
+    }
+}
