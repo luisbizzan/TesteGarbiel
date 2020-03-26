40bf29cc34de61a6e68844a61928e1d1c0f17cf5
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-03-26 09:50:07 -0300

    FUR-1227 - Inclusão de Api.

---
+++ FWLog.Web.Api/Models/Armazenagem/NivelPontoArmazenagemPorCorredorResposta.cs
@@ -0,0 +1,9 @@
+﻿namespace FWLog.Web.Api.Models.Armazenagem
+{
+    public class NivelPontoArmazenagemPorCorredorResposta
+    {
+        public long IdEnderecoArmazenagem { get; set; } 
+        public string NivelArmazenagemDescricao { get; set; }
+        public string PontoArmazenagemDescricao { get; set; }
+    }
+}
