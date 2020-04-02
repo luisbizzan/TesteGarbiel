f47556d2381c4e40bd5285fc831f94e8286d54f9
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-04-02 11:03:55 -0300

    Controller de Atividade de Estoque

---
+++ FWLog.Services/Services/AtividadeEstoqueService.cs
@@ -0,0 +1,14 @@
+﻿using FWLog.Data;
+
+namespace FWLog.Services.Services
+{
+    public class AtividadeEstoqueService : BaseService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public AtividadeEstoqueService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ FWLog.Web.Api/Controllers/AtividadeEstoqueController.cs
@@ -0,0 +1,16 @@
+﻿using FWLog.Data;
+using FWLog.Services.Services;
+
+namespace FWLog.Web.Api.Controllers
+{
+    public class AtividadeEstoqueController : ApiBaseController
+    {
+        private AtividadeEstoqueService _atividadeEstoqueService;
+        private UnitOfWork _unitOfWork;
+        public AtividadeEstoqueController(UnitOfWork unitOfWork, AtividadeEstoqueService atividadeEstoqueService)
+        {
+            _unitOfWork = unitOfWork;
+            _atividadeEstoqueService = atividadeEstoqueService;
+        }
+    }
+}
