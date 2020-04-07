bb0005433a349b1f0a29ed418d5ad9e677021114
popolinclaudio <claudio.popolin@dartdigital.com.br>
2020-04-07 16:59:48 -0300

    Implementação da tarefa FUR-1288

---
+++ FWLog.Services/Model/Relatorios/ImprimirRelatorioTotalPorAlaRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace FWLog.Services.Model.Relatorios
+{
+    public class ImprimirRelatorioTotalPorAlaRequest
+    {
+        public long? IdEmpresa { get; set; }
+        public string NomeUsuarioRequisicao { get; set; }
+        public long IdNivelArmazenagem { get; set; }
+        public long IdPontoArmazenagem { get; set; }
+        public int? CorredorInicial { get; set; }
+        public int? CorredorFinal { get; set; }
+        public bool ImprimirVazia { get; set; }
+        public int IdImpressora { get; set; }
+    }
+}
