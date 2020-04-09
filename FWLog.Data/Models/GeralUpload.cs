ad6d0b784d4618202550a04eccd9d88d92ce0709
Henrique D Pereira <henriquedpereira@hotmail.com>
2020-04-09 11:00:18 -0300

    upload generico

---
+++ FWLog.Data/Models/GeralUpload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FWLog.Data.Models
+{
+    public class GeralUpload
+    {
+        public long Id { get; set; }
+        public long Id_Categoria { get; set; }
+        public long Id_Ref { get; set; }
+        public string Id_Usr { get; set; }
+        public string Arquivo { get; set; }
+        public string Arquivo_Tipo { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Dt_Cad { get; set; }
+    }
+
+    public class GeralUploadCategoria
+    {
+        public long Id { get; set; }
+        public string Tabela { get; set; }
+        public string Categoria { get; set; }
+        public string Formatos { get; set; }
+    }
+}
---
+++ FWLog.Web.Backoffice/Models/GeralCtx/GeralHistoricoVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FWLog.Web.Backoffice.Models.GeralCtx
+{
+    public class GeralHistoricoVM
+    {
+        [Required]
+        public long Id_Categoria { get; set; }
+
+        [Required]
+        public long Id_Ref { get; set; }
+
+        public string Id_Usr { get; set; }
+
+        [Required]
+        public string Historico { get; set; }
+
+        public string Usuario { get; set; }
+
+        public DateTime Dt_Cad { get; set; }
+
+        public List<GeralHistoricoVM> Lista_Historicos { get; set; }
+    }
+}
