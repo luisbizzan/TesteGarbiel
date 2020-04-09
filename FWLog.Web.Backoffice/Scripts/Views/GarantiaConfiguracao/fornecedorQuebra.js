6e1c159a4c2a8def579c2acf5acb3545b2d708a7
Luciano <ti03@furacao.com.br>
2020-04-09 14:46:56 -0300

    - inclusão de view

---
+++ FWLog.Web.Backoffice/Scripts/Views/GarantiaConfiguracao/fornecedorQuebra.js
@@ -0,0 +1,7 @@
+﻿
+/* gravar novo fornecedor quebra */
+$("#btnGravar").click(function (e) {
+    //PNotify.success({ text: "mensagem de sucesso." });
+    //$.toaster({ priority: "danger", title: "Erro", message: "Falha ao salvar!" });
+    console.log("aqui");
+});
