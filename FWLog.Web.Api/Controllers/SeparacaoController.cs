7649ff9f5b60253872d54f947ab09f8234c2e43f
popolinclaudio <claudio.popolin@dartdigital.com.br>
2020-04-22 17:15:04 -0300

    Commit inicial da tarefa FUR-1270

---
+++ FWLog.Web.Api/Controllers/SeparacaoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FWLog.Web.Api.Controllers
+{
+    public class SeparacaoController : ApiBaseController
+    {
+        //[Route("api/v1/separacao/separacao-pedido/buscar-pedido-venda")]
+        //[HttpPost]
+        //public IHttpActionResult BuscarPedidoVenda(ValidarLoteProdutoInstalacaoModelRequisicao requisicao)
+        //{
+           
+        //}
+    }
+}
