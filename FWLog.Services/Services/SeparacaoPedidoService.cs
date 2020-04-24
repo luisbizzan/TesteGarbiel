942cee5bb6410aa5a29c0bb81789880580cc7310
Vinícius Haga <vinicius.haga@dartdigital.com.br>
2020-04-24 10:26:13 -0300

    Método para atualizar o status de pedido no Sankhya

--- FWLog.Services/Services/SeparacaoPedidoService.cs
+++ FWLog.Services/Services/SeparacaoPedidoService.cs
@@ -1,13 +1,11 @@
 ﻿using DartDigital.Library.Exceptions;
 using FWLog.Data;
 using FWLog.Data.Models;
-using FWLog.Data.Models.DataTablesCtx;
-using FWLog.Services.Model.Armazenagem;
-using FWLog.Services.Model.Coletor;
+using FWLog.Services.Integracao;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FWLog.Services.Services
@@ -15,16 +13,40 @@ namespace FWLog.Services.Services
     public class SeparacaoPedidoService
     {
         private readonly UnitOfWork _unitOfWork;
+        private ILog _log;
 
-        public SeparacaoPedidoService(UnitOfWork unitOfWork)
+        public SeparacaoPedidoService(UnitOfWork unitOfWork, ILog log)
         {
             _unitOfWork = unitOfWork;
+            _log = log;
         }
 
         public List<long> ConsultaPedidoVendaEmSeparacao(string idUsuario, long idEmpresa)
         {
             var ids = _unitOfWork.PedidoVendaRepository.PesquisarIdsEmSeparacao(idUsuario, idEmpresa);
             return ids;
+        }
+
+        public async Task AtualizarStatusPedidoVenda(PedidoVenda pedidoVenda, PedidoVendaStatusEnum statusPedidoVenda)
+        {
+            if (!Convert.ToBoolean(ConfigurationManager.AppSettings["IntegracaoSankhya_Habilitar"]))
+            {
+                return;
+            }
+
+            try
+            {
+                Dictionary<string, string> campoChave = new Dictionary<string, string> { { "NUNOTA", pedidoVenda.CodigoIntegracao.ToString() } };
+
+                await IntegracaoSankhya.Instance.AtualizarInformacaoIntegracao("CabecalhoNota", campoChave, "AD_STATUSSEP", statusPedidoVenda.GetHashCode());
+
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = string.Format("Erro na atualização do pedido de venda: {0}.", pedidoVenda.CodigoIntegracao);
+                _log.Error(errorMessage, ex);
+                throw new BusinessException(errorMessage);
+            }
         }
     }
 }
