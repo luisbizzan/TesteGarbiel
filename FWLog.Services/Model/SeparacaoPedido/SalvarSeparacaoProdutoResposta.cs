60feab018cdfc57e8e82313b3f0c46e9e985f452
Gabriel Bonacim Antonio <biel_bonacim@hotmail.com>
2020-04-29 08:01:30 -0300

    Remoção campo nullable

--- FWLog.Services/Model/SeparacaoPedido/SalvarSeparacaoProdutoResposta.cs
+++ FWLog.Services/Model/SeparacaoPedido/SalvarSeparacaoProdutoResposta.cs
@@ -7,7 +7,7 @@
         public string Referencia { get; set; }
         public int QtdSeparar { get; set; }
         public int QtdSeparada { get; set; }
-        public decimal? Multiplo { get; set; }
+        public decimal Multiplo { get; set; }
         public bool ProdutoSeparado { get; set; }
         public bool VolumeSeparado { get; set; }
     }
