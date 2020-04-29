d6b020e34dd2dead13737b5d0bd6c6e8cf91d911
Gabriel Bonacim Antonio <biel_bonacim@hotmail.com>
2020-04-29 16:34:41 -0300

    Validação de produto em Salvar Separação Produto

--- FWLog.Web.Api/Models/SeparacaoPedido/SalvarSeparacaoProdutoRequisicao.cs
+++ FWLog.Web.Api/Models/SeparacaoPedido/SalvarSeparacaoProdutoRequisicao.cs
@@ -4,9 +4,13 @@ namespace FWLog.Web.Api.Models.SeparacaoPedido
 {
     public class SalvarSeparacaoProdutoRequisicao
     {
-        [Required(ErrorMessage = "O id do pedido venda deve ser informado.")]
+        [Required(ErrorMessage = "O IdPedidoVenda deve ser informado.")]
         public long IdPedidoVenda { get; set; }
-        [Required(ErrorMessage = "O id do produto deve ser informado.")]
+
+        [Required(ErrorMessage = "O IdProduto deve ser informado.")]
         public long IdProduto { get; set; }
+
+        [Required(ErrorMessage = "O IdProdutoSeparacao deve ser informado.")]
+        public long IdProdutoSeparacao { get; set; }
     }
 }
