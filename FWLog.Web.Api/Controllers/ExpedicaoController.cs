342f315e1a49aced069cbc88761f82cc3516cbd1
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-05-04 10:09:47 -0300

    Criação do service e controller para expedicao

---
+++ FWLog.Web.Api/Controllers/ExpedicaoController.cs
@@ -0,0 +1,14 @@
+﻿using FWLog.Services.Services;
+
+namespace FWLog.Web.Api.Controllers
+{
+    public class ExpedicaoController : ApiBaseController
+    {
+        private readonly ExpedicaoService _expedicaoService;
+
+        public ExpedicaoController(ExpedicaoService expedicaoService)
+        {
+            _expedicaoService = expedicaoService;
+        }
+    }
+}
