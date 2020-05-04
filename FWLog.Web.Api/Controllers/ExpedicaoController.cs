f5601ef87208287a6f6a521c256267eb2ca8af48
Fernanda Neves Felix Silva Franco <fernanda.neves@dartdigital.com.br>
2020-05-04 14:07:25 -0300

    Inclusão de validação de volume para expedição.

--- FWLog.Web.Api/Controllers/ExpedicaoController.cs
+++ FWLog.Web.Api/Controllers/ExpedicaoController.cs
@@ -1,14 +1,35 @@
-﻿using FWLog.Services.Services;
+﻿using DartDigital.Library.Exceptions;
+using FWLog.Data;
+using FWLog.Services.Services;
+using System.Web.Http;
 
 namespace FWLog.Web.Api.Controllers
 {
     public class ExpedicaoController : ApiBaseController
     {
         private readonly ExpedicaoService _expedicaoService;
+        private readonly UnitOfWork _unitOfWork;
 
-        public ExpedicaoController(ExpedicaoService expedicaoService)
+        public ExpedicaoController(ExpedicaoService expedicaoService, UnitOfWork unitOfWork)
         {
             _expedicaoService = expedicaoService;
+            _unitOfWork = unitOfWork;
+        }
+
+        [Route("api/v1/expedicao/validar-referencia-pedido/{referenciaPedido}")]
+        [HttpPost]
+        public IHttpActionResult ValidarReferenciaPedido(string referenciaPedido)
+        {
+            try
+            {
+                 _expedicaoService.ValidarReferenciaPedido(referenciaPedido, IdEmpresa);
+
+                return ApiOk();
+            }
+            catch (BusinessException ex)
+            {
+                return ApiBadRequest(ex.Message);
+            }
         }
     }
 }
