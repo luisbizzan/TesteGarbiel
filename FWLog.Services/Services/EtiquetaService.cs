086ab8e8f7a3f0a8ceb09a1c95abfe32ec5f2e35
Gabriel Bonacim Antonio <biel_bonacim@hotmail.com>
2020-05-13 16:15:57 -0300

    Ajustes etiqueta picking

--- FWLog.Services/Services/EtiquetaService.cs
+++ FWLog.Services/Services/EtiquetaService.cs
@@ -541,9 +541,9 @@ namespace FWLog.Services.Services
 
             etiquetaZpl.AppendLine($"^FO55,85^FB430,1,0,C,0^A0B,250,60^FR^FD{referenciaProduto}^FS");
 
-            etiquetaZpl.AppendLine($"^FO430,30^A0B,180,120^FD{codigoEndereco}^FS");
+            etiquetaZpl.AppendLine($"^FO370,30^A0B,180,120^FD{codigoEndereco}^FS");
 
-            etiquetaZpl.AppendLine($"^FO600,100^BCR,85,N,N^FD{idEnderecoFormatado}^FS");
+            etiquetaZpl.AppendLine($"^FO600,180^BCR,100,N,N^FD{idEnderecoFormatado}^FS");
 
             etiquetaZpl.AppendLine($"^XZ");
 
