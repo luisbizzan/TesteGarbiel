dd55fdf8bff706ca67f95e56fec60fdd1e2a1897
victorozorio <victor.ozorio@dartdigital.com.br>
2020-06-01 12:09:47 -0300

    commit tarefa FUR-1735

--- FWLog.Web.Backoffice/Scripts/Views/ApplicationLog/details.js
+++ FWLog.Web.Backoffice/Scripts/Views/ApplicationLog/details.js
@@ -1,11 +1,3 @@
 ﻿(function () {
     
 })();
-
-function expandTextarea(id) {
-    document.getElementById(id).addEventListener('keyup', function () {
-        this.style.overflow = 'hidden';
-        this.style.height = 0;
-        this.style.height = this.scrollHeight + 'px';
-    }, false);
-}
