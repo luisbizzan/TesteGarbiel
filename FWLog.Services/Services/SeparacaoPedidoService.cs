b64d0196096426bdb57207139f788a9c5c137da0
victorozorio <victor.ozorio@dartdigital.com.br>
2020-06-16 14:36:11 -0300

    Removi o Where da lista de pedidos (havia esquecido)

--- FWLog.Services/Services/SeparacaoPedidoService.cs
+++ FWLog.Services/Services/SeparacaoPedidoService.cs
@@ -993,7 +993,7 @@ namespace FWLog.Services.Services
             var grupoCorredorArmazenagem = _unitOfWork.GrupoCorredorArmazenagemRepository.Todos().Where(x => x.IdEmpresa == idEmpresa).OrderBy(x => x.CorredorInicial).ToList();
 
             //Captura os pedidos por empresa e status pendente separação.
-            var listaPedidos = _unitOfWork.PedidoRepository.PesquisarPendenteSeparacao(idEmpresa).Where(x => x.IdPedido == 261);
+            var listaPedidos = _unitOfWork.PedidoRepository.PesquisarPendenteSeparacao(idEmpresa);
 
             foreach (var pedido in listaPedidos) //Percorre a lista de pedidos.
             {
