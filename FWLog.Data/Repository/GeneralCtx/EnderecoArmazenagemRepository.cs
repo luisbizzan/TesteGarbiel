2256868da3913113dfa3fb22055e394585b6b619
Gabriel Bonacim Antonio <biel_bonacim@hotmail.com>
2020-06-24 13:19:38 -0300

    Ajustes regras de picking em edição produto e validação instalação

--- FWLog.Data/Repository/GeneralCtx/EnderecoArmazenagemRepository.cs
+++ FWLog.Data/Repository/GeneralCtx/EnderecoArmazenagemRepository.cs
@@ -84,7 +84,7 @@ namespace FWLog.Data.Repository.GeneralCtx
                          (filtros.CustomFilter.Codigo.Equals(string.Empty) || e.Codigo.Contains(filtros.CustomFilter.Codigo)) &&
                          (filtros.CustomFilter.IdPontoArmazenagem.HasValue == false || e.IdPontoArmazenagem == filtros.CustomFilter.IdPontoArmazenagem) &&
                          (filtros.CustomFilter.BuscarTodos == true || (!(from p in Entities.ProdutoEstoque where p.IdEnderecoArmazenagem == e.IdEnderecoArmazenagem select p.IdEnderecoArmazenagem).Any() && e.Ativo == true &&
-                         e.IsPontoSeparacao == true)) &&
+                         e.IsPicking == true)) &&
                          (filtros.CustomFilter.IsExpedicao.HasValue == false || !(from te in Entities.TransportadoraEndereco where te.IdEnderecoArmazenagem == e.IdEnderecoArmazenagem select te.IdEnderecoArmazenagem).Any() && e.IsFifo == false && e.IsPontoSeparacao == false && e.IsPicking == false)
                          select new EnderecoArmazenagemPesquisaModalListaLinhaTabela
                          {
